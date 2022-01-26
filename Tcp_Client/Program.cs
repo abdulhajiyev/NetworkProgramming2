c46dd57dc5887400119dc471f3617eff26b0ebe0
Abdul Hajiyev <abdulhajiyev@gmail.com>
2022-01-26 20:55:30 +0400

    smChgns

--- Tcp_Client/Program.cs
+++ Tcp_Client/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using System.Net;
+using System.Net.Sockets;
+
+var ip = IPAddress.Loopback;
+var client = new TcpClient();
+client.Connect(ip, 45001);
+var stream = client.GetStream();
+var binaryWriter = new BinaryWriter(stream);
--- Tcp_Server/Program.cs
+++ Tcp_Server/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var ip = IPAddress.Loopback;
+var listener = new TcpListener(ip, 45001);
+
+listener.Start(100);
+Console.WriteLine("Listening on {0}", listener.LocalEndpoint);
+
+while (true)
+{
+    listener.AcceptTcpClient();
+    Console.WriteLine("Client connected");
+
+    var client = listener.AcceptTcpClient();
+    var stream = client.GetStream();
+    var binaryWriter = new BinaryWriter(stream);
+    var binaryReader = new BinaryReader(stream);
+
+    while (true)
+    {
+        var message = binaryReader.ReadString();
+        Console.WriteLine(message);
+        binaryWriter.Write("Hakuna Matata");
+    }
+}
