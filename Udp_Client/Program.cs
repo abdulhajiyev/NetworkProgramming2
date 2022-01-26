db1708b3dad49d89289d9ae0ff211995ef11a7fb
Abdul Hajiyev <abdulhajiyev@gmail.com>
2022-01-26 19:54:12 +0400

    .

---
+++ Udp_Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var client = new Socket(
+    AddressFamily.InterNetwork,
+    SocketType.Dgram,
+    ProtocolType.Udp);
+
+var ip = IPAddress.Loopback;
+var serverEp = new IPEndPoint(ip, 45678);
+
+while (true)
+{
+    var message = Console.ReadLine();
+    var data = Encoding.Default.GetBytes(message);
+    client.SendTo(data, serverEp);
+}
---
+++ Udp_Server/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var listener = new Socket(
+    AddressFamily.InterNetwork,
+    SocketType.Dgram,
+    ProtocolType.Udp);
+
+var ip = IPAddress.Loopback;
+var listenerEp = new IPEndPoint(ip, 45678);
+
+listener.Bind(listenerEp);
+var buffer = new byte[ushort.MaxValue];
+
+EndPoint remoteEp = new IPEndPoint(IPAddress.Any, 0);
+
+while (true)
+{
+    var bytesReceived = listener.ReceiveFrom(buffer, ref remoteEp);
+    var message = Encoding.Default.GetString(buffer, 0, bytesReceived);
+    Console.WriteLine($"Received: {message}");
+}
