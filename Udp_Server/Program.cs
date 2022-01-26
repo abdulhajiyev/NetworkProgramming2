6365a0df446651f6510a373b620c9f609685453e
Abdul Hajiyev <abdulhajiyev@gmail.com>
2022-01-26 20:08:27 +0400

    Tcp

--- Udp_Server/Program.cs
+++ Udp_Server/Program.cs
@@ -2,22 +2,33 @@
 using System.Net.Sockets;
 using System.Text;
 
-var listener = new Socket(
-    AddressFamily.InterNetwork,
-    SocketType.Dgram,
-    ProtocolType.Udp);
-
-var ip = IPAddress.Loopback;
-var listenerEp = new IPEndPoint(ip, 45678);
-
-listener.Bind(listenerEp);
-var buffer = new byte[ushort.MaxValue];
-
-EndPoint remoteEp = new IPEndPoint(IPAddress.Any, 0);
-
-while (true)
+class Program
 {
-    var bytesReceived = listener.ReceiveFrom(buffer, ref remoteEp);
-    var message = Encoding.Default.GetString(buffer, 0, bytesReceived);
-    Console.WriteLine($"Received: {message}");
+    static void Main(string[] args) => MainAsync(args).GetAwaiter().GetResult();
+    
+    static async Task MainAsync(string[] args)
+    {
+        var listener = new Socket(
+            AddressFamily.InterNetwork,
+            SocketType.Dgram,
+            ProtocolType.Udp);
+
+        var ip = IPAddress.Loopback;
+        var listenerEp = new IPEndPoint(ip, 45678);
+
+        listener.Bind(listenerEp);
+        var buffer = new byte[ushort.MaxValue];
+        var segment = new ArraySegment<byte>(buffer);
+
+        EndPoint remoteEp = new IPEndPoint(IPAddress.Any, 0);
+
+        while (true)
+        {
+            var bytesReceived = await listener.ReceiveFromAsync(segment, SocketFlags.None, remoteEp);
+            var len = bytesReceived.ReceivedBytes;
+            var endPoint = bytesReceived.RemoteEndPoint;
+            var message = Encoding.Default.GetString(buffer, 0, len);
+            Console.WriteLine($"Received: {message}");
+        }
+    }
 }
